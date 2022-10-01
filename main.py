1b1306e7c7f8cc7c4f55231241d4982aace80267
daniya_kuspanova <kyspanova@icloud.com>
2022-10-01 20:40:56 +0400

    попытка получения доступа к сайту кинопоиск и к рецензиям

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print(1)
+import os
+
+import requests
+from bs4 import BeautifulSoup, Beautyfulsoup
+#import re
+#from re import sub
+#from decimal import Decimal
+#import io
+#from datetime import datetime
+#import pandas as pd
+
+URL = "https://www.kinopoisk.ru/film/435/reviews/"
+html_text= requests.get(URL).text
+soup = BeautifulSoup(html_text, 'lxml')
+soup.fond('div', class_ = 'reviewItem userReview')
+#html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
+# html_page.text - хранит html код веб-страницы
