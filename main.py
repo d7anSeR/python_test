2dd052512a3b1ad7c3a823197e61c167ce02106a
daniya_kuspanova <kyspanova@icloud.com>
2022-10-11 00:48:02 +0400

    настройка открытия файла

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from random import randint
 
 URL = 'https://www.kinopoisk.ru/film/435/reviews/'
 html_text = requests.get(URL).text
-sleep(randint(3,10))
+sleep(2)
 
 soup = BeautifulSoup(html_text, features = "lxml")
 i = 0
@@ -18,20 +18,19 @@ review = soup.find('div', class_= 'reviewItem userReview')
 reviewsAll = soup.findAll("div", {"class":"reviewItem userReview"})
 print(len(reviewsAll))
 p = 1
+i = 0
 for p in range(1, 46):
     url = f"https://www.kinopoisk.ru/film/435/reviews/ord/date/status/all/perpage/10/page/{p}/"
     req = requests.get(url)
-    sleep(3)
-    i = 0
+    sleep(1)
     soup = BeautifulSoup(req.text, 'lxml')
     for review in reviewsAll:
-        #feedback = soup.find('span', class_ ='_reachbanner_').text
+        feedback = soup.find('span', class_ ='_reachbanner_')
         if soup.find('span', class_ = 'yes'):
-          nature_review = '\good'
+          nature_review = "\good\\"
         if soup.find('span', class_ = 'no'):
-          nature_review = '\bad'
-        #wayfile = 'C:\dataset' + nature_review + i.zfill(4)
-        #f = open(wayfile, 'w')
+          nature_review = "\bad\\"
+        f = open("C:\dataset" + nature_review + i.zfill(4), 'w')
         data.insert(i, i)
         i += 1
 print(data)
