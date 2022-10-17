20e56697ef665c2879c4dca6cfb4c35f5a3fd747
daniya_kuspanova <kyspanova@icloud.com>
2022-10-17 19:43:04 +0400

    создание мейна, добавление фильмов

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 import os
+import string
 from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.kinopoisk.ru/film/435/reviews/'
-html_text = requests.get(URL).text
-sleep(1)
 
-soup = BeautifulSoup(html_text, features="lxml")
-i = 0
-review = soup.find('div', class_='reviewItem userReview')
-reviewsAll = soup.findAll("div", {"class": "reviewItem userReview"})
-print(len(reviewsAll))
-title_film = soup.find("a", {"class": "breadcrumbs__link"})
-print(title_film)
-i = 0
-for p in range(1, 46):
-    url = f"https://www.kinopoisk.ru/film/435/reviews/ord/date/status/all/perpage/10/page/{p}/"
-    req = requests.get(url)
-    sleep(3)
-    soup = BeautifulSoup(req.text, 'lxml')
-    for r in reviewsAll:
-        feedback = soup.find('span', class_='_reachbanner_').text
-        if soup.find('span', class_='yes'):
-            number = (i+1).zfill(4)
-            with open('dataset/good/{number}.txt', mode = 'w') as f:
-                f.write(title_film)
-                f.write('\n')
-                f.write(feedback)
-            pass
-        if soup.find('span', class_='no'):
-            number = (i+1).zfill(4)
-            with open('dataset/bad/{number}.txt', mode = 'w') as f:
-                f.write(title_film)
-                f.write('\n')
-                f.write(feedback)
-            pass
-        i += 1
-print("done")
+def film(n, data_good, data_bad, link_site, link_page):
+    URL = link_site
+    html_text = requests.get(URL).text
+    sleep(8)
+    soup = BeautifulSoup(html_text, features="lxml")
+    review = soup.find('div', class_='reviewItem userReview')
+    title_film = soup.find("a", {"class": "breadcrumbs__link"}).text
+    for p in range(1, n):
+        url = link_page+"{p}/"
+        req = requests.get(url)
+        sleep(5)
+        soup = BeautifulSoup(req.text, 'lxml')
+        reviewsAll = soup.findAll("div", {"class": "reviewItem userReview"})
+        print(len(reviewsAll))
+        for review in reviewsAll:
+            feedback = review.find("span", {"class": "_reachbanner_"}).text
+            print(feedback)
+            if review.find('span', class_='yes'):
+                if(data_good < 1000):
+                    number = chr(data_good+1).zfill(4)
+                    data_good + 1
+                    with open(r"C:\Users\Professional\.vscode\python_test\dataset\good\{number}.txt", 'a') as f:
+                        f.write(title_film)
+                        f.write('\n')
+                        f.write(feedback)
+                    pass       
+            if review.find('span', class_='no'):
+                if(data_bad < 1000):
+                    number = chr(data_bad+1).zfill(4)
+                    data_bad + 1
+                    with open(r"C:\Users\Professional\.vscode\python_test\python_test\python_test\dataset\bad\{number}.txt", 'a') as f:
+                        f.write(title_film)
+                        f.write('\n')
+                        f.write(feedback)
+                    pass
+data_good = data_bad = 0
+film(41,data_good, data_bad, 'https://www.kinopoisk.ru/film/435/reviews/', 'https://www.kinopoisk.ru/film/435/reviews/ord/date/status/all/perpage/10/page/')
+film(27,data_good, data_bad, 'https://www.kinopoisk.ru/film/1294123/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/1294123/reviews/ord/rating/status/all/perpage/10/page/')
+film(13, data_good, data_bad, 'https://www.kinopoisk.ru/film/11130/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/11130/reviews/ord/rating/status/all/perpage/10/page/')
+film(7, data_good, data_bad, 'https://www.kinopoisk.ru/film/964318/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/964318/reviews/ord/rating/status/all/perpage/10/page/')
+film(55, data_good, data_bad, 'https://www.kinopoisk.ru/film/535341/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/535341/reviews/ord/rating/status/all/perpage/10/page/')
+film(237, data_good, data_bad, 'https://www.kinopoisk.ru/film/251733/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/251733/reviews/ord/rating/status/all/perpage/10/page/')
+film(119, data_good, data_bad, 'https://www.kinopoisk.ru/film/258687/reviews/', 'https://www.kinopoisk.ru/film/258687/reviews/ord/date/status/all/perpage/10/page/')
+film(30, data_good, data_bad, 'https://www.kinopoisk.ru/film/689/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/689/reviews/ord/rating/status/all/perpage/10/page/')
+film(102, data_good, data_bad, 'https://www.kinopoisk.ru/film/111543/reviews/ord/rating/', 'https://www.kinopoisk.ru/film/111543/reviews/ord/rating/status/all/perpage/10/page/')
+print(data_bad, data_good)
