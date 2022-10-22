97e7c40152827c1e75d60990b68c4654fa7f8bcb
daniya_kuspanova <kyspanova@icloud.com>
2022-10-22 16:43:45 +0400

    итоговый вариант лабы:утверждение нужного количества фильмов

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def review_good(data_good, pages, link_page):
     for page in range(1, pages+1):
         print(page)
         url = link_page + f"{pages}/"
-        sleep(30 + 2*pages)
+        sleep(30 + 3*page)
         req = requests.get(url)
         req.encoding = 'UTF-8'
         soup = BeautifulSoup(req.text, 'lxml')
@@ -28,10 +28,11 @@ def review_good(data_good, pages, link_page):
             title_film = soup.find("a", class_="breadcrumbs__link").text
         except:
             print("error when parsng the website")
+            return
         reviewsAll = soup.findAll("div", class_="response good")
         for review in reviewsAll:
             feedback = review.find("span", {"class": "_reachbanner_"}).text
-            if data_good <= 1000:
+            if data_good < 1000:
                 number = str(data_good+1).zfill(4)
                 data_good += 1
             try:
@@ -55,10 +56,11 @@ def review_bad(data_bad, pages, link_page):
             title_film = soup.find("a", class_="breadcrumbs__link").text
         except:
             print("error when parsng the website")
+            return
         reviewsAll = soup.findAll("div", class_="response bad")
         for review in reviewsAll:
             feedback = review.find("span", {"class": "_reachbanner_"}).text
-            if data_bad <= 1000:
+            if data_bad < 1000:
                 number = str(data_bad+1).zfill(4)
                 data_bad += 1
             try:
@@ -70,28 +72,32 @@ def review_bad(data_bad, pages, link_page):
     return data_bad
 
 
-def run(n, _data_good, _data_bad, link_page):
+def run():
+    _data_good = _data_bad = 0
     make_dir()
-    _data_good = review_good(_data_good, n, link_page)
-    _data_bad = review_bad(_data_bad, n, link_page)
-    return _data_good, _data_bad
+    _data_good = review_good(
+        _data_good, 50, 'https://www.kinopoisk.ru/film/326/reviews/ord/date/status/good/perpage/10/page/')
+    _data_good = review_good(
+        _data_good, 42, 'https://www.kinopoisk.ru/film/448/reviews/ord/date/status/good/perpage/10/page/')
+    _data_good = review_good(
+        _data_good, 12, 'https://www.kinopoisk.ru/film/11130/reviews/ord/date/status/good/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 43, 'https://www.kinopoisk.ru/film/401177/reviews/ord/date/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 8, 'https://www.kinopoisk.ru/film/395060/reviews/ord/date/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 42, 'https://www.kinopoisk.ru/film/251733/reviews/ord/rating/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 13, 'https://www.kinopoisk.ru/film/470179/reviews/ord/date/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 7, 'https://www.kinopoisk.ru/film/464963/reviews/ord/date/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 7, 'https://www.kinopoisk.ru/film/464963/reviews/ord/date/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 37, 'https://www.kinopoisk.ru/film/714888/reviews/ord/date/status/bad/perpage/10/page/')
+    _data_bad = review_bad(
+        _data_bad, 10, 'https://www.kinopoisk.ru/film/718222/reviews/ord/date/status/bad/perpage/10/page/')
 
 
 if __name__ == '__main__':
-    _data_good = _data_bad = 0
-    _data_good = _data_bad = run(
-        27, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/1294123/reviews/ord/rating/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        13, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/11130/reviews/ord/rating/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        7, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/964318/reviews/ord/rating/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        55, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/535341/reviews/ord/rating/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        237, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/251733/reviews/ord/rating/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        119, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/258687/reviews/ord/date/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        30,  _data_good, _data_bad, 'https://www.kinopoisk.ru/film/689/reviews/ord/rating/status/all/perpage/10/page/')
-    _data_good = _data_bad = run(
-        102, _data_good, _data_bad, 'https://www.kinopoisk.ru/film/111543/reviews/ord/rating/status/all/perpage/10/page/')
+    run()
